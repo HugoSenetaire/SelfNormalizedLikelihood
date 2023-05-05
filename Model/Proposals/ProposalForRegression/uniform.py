0a44934606f8c66848ebfaca3086db84e204bb50
Hugo <hhjs@dtu.dk>
2023-05-05 19:06:56 +0200

    fix : uniform proposal issues

--- Model/Proposals/ProposalForRegression/uniform.py
+++ Model/Proposals/ProposalForRegression/uniform.py
@@ -4,26 +4,35 @@ import torch
 import torch.nn as nn
 
 class UniformRegression(nn.Module):
-    def __init__(self, input_size_x, input_size_y, dataset, min='dataset', max ='dataset', **kwargs) -> None:
+    def __init__(self, input_size_x, input_size_y, dataset, min_data='dataset', max_data ='dataset', **kwargs) -> None:
         super().__init__()
         self.input_size_x = np.prod(input_size_x)
         self.input_size_y = np.prod(input_size_y)
         print("Init Standard Gaussian...")
-        index = np.random.choice(len(dataset), 100)
-        data = torch.cat([dataset[i][1].reshape(1, self.input_size_y) for i in index])
-        if min == 'dataset' :
-            self.min = data.min(0).values
-        else :
-            self.min = torch.tensor(min, dtype=torch.float32)
-        if max == 'dataset' :
-            self.max = data.max(0).values
-        else :
-            self.max = torch.tensor(max, dtype=torch.float32)
 
-        self.min = nn.parameter.Parameter(self.min,)
-        self.max = nn.parameter.Parameter(self.max,)
-        
-        self.distribution = dist.Uniform(self.min, self.max)
+        if min_data == 'dataset' :
+            self.min_data = float('inf') 
+            for i in range(len(dataset)):
+                self.min_data = min(self.min_data, dataset[i][1])
+
+            self.min_data = torch.tensor(self.min_data -1, dtype=torch.float32)
+        else :
+            self.min_data = torch.tensor(min_data, dtype=torch.float32)
+        if max_data == 'dataset' :
+            self.max_data = float('-inf') 
+            for i in range(len(dataset)):
+                self.max_data = max(self.max_data, dataset[i][1])
+            self.max_data= torch.tensor(self.max_data +1, dtype=torch.float32)
+        else :
+            self.max_data = torch.tensor(max_data, dtype=torch.float32)
+
+        self.min_data = nn.parameter.Parameter(self.min_data,)
+        self.max_data = nn.parameter.Parameter(self.max_data,)
+
+
+        print("Minimum data for uniform proposal : ", self.min_data)
+        print("Maximum data for uniform proposal : ", self.max_data)
+        self.distribution = dist.Uniform(self.min_data, self.max_data)
         print("Init Standard Gaussian... end")
 
     def sample(self, x_feature, nb_sample = 1):
