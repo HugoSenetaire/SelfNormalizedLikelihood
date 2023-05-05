e09ae4c86bd299f66dab848ce47cbbc176682ce1
PaulJeha <paul.jeha@gmail.com>
2023-05-05 22:41:32 +0200

    Implementing experiment on discrete data

---
+++ Model/Proposals/ProposalForDistributionEstimation/categorical.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+from jaxtyping import Float
+from torch.distributions import categorical
+from torch.utils.data import Dataset
+
+
+class Categorical(nn.Module):
+    """Module that implements a uniform categorical distribution"""
+
+    def __init__(self, input_size: Tuple[int], dataset: Dataset) -> None:
+        self.input_size = input_size
+        self.num_classes = dataset.num_classes
+        self.distribution = categorical.Categorical(
+            torch.ones(self.num_classes) / self.num_classes
+        )
+
+    def sample(self, nb_sample: int = 1):
+        samples = self.distribution.sample((nb_sample, 1, 1))
+        return samples
+
+    def log_prob(self, x: Float[torch.Tensor, "batch_size 1 1"]):
+        return self.distribution.log_prob(x).flatten(1).sum(1)
--- Model/Proposals/proposal_getter.py
+++ Model/Proposals/proposal_getter.py
@@ -1,32 +1,39 @@
-from .ProposalForDistributionEstimation.standard_gaussian import StandardGaussian
-from .ProposalForDistributionEstimation.kde import KernelDensity
+from .ProposalForDistributionEstimation.categorical import Categorical
 from .ProposalForDistributionEstimation.gaussian_mixture import GaussianMixtureProposal
-from .ProposalForRegression.standard_gaussian import StandardGaussianRegression
+from .ProposalForDistributionEstimation.kde import KernelDensity
+from .ProposalForDistributionEstimation.poisson import Poisson
+from .ProposalForDistributionEstimation.standard_gaussian import StandardGaussian
 from .ProposalForRegression.MDNProposal import MDNProposalRegression
+from .ProposalForRegression.standard_gaussian import StandardGaussianRegression
 
 dic_proposals = {
-    'standard_gaussian': StandardGaussian,
-    'kernel_density': KernelDensity,
-    'gaussian_mixture': GaussianMixtureProposal,
+    "standard_gaussian": StandardGaussian,
+    "kernel_density": KernelDensity,
+    "gaussian_mixture": GaussianMixtureProposal,
+    "poisson": Poisson,
+    "uniform_categorical": Categorical,
 }
 
 
 def get_proposal(args_dict, input_size, dataset):
-    proposal = dic_proposals[args_dict['proposal_name']]
-    if 'proposal_params' in args_dict:
-        return proposal(input_size, dataset, **args_dict['proposal_params'])
+    proposal = dic_proposals[args_dict["proposal_name"]]
+    if "proposal_params" in args_dict:
+        return proposal(input_size, dataset, **args_dict["proposal_params"])
     else:
         return proposal(input_size, dataset)
-    
+
 
 dic_proposals_regression = {
-    'standard_gaussian': StandardGaussianRegression,
-    'mdn': MDNProposalRegression,
+    "standard_gaussian": StandardGaussianRegression,
+    "mdn": MDNProposalRegression,
 }
-    
+
+
 def get_proposal_regression(args_dict, input_size_x, input_size_y, dataset):
-    proposal = dic_proposals_regression[args_dict['proposal_name']]
-    if 'proposal_params' in args_dict:
-        return proposal(input_size_x, input_size_y, dataset, **args_dict['proposal_params'])
+    proposal = dic_proposals_regression[args_dict["proposal_name"]]
+    if "proposal_params" in args_dict:
+        return proposal(
+            input_size_x, input_size_y, dataset, **args_dict["proposal_params"]
+        )
     else:
         return proposal(input_size_x, input_size_y, dataset)
