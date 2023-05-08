c74a31a70023f4a94ec6ee5eea85d3097e6665b2
HugoSenetaire <hhjs@dtu.dk>
2023-05-08 17:42:15 +0200

    feat : allow for very large sampling for Z estimation without memory issues

--- Model/EBMsAndMethod/Regression/global_ebm.py
+++ Model/EBMsAndMethod/Regression/global_ebm.py
@@ -2,7 +2,7 @@
 import torch.nn as nn
 import torch
 import torch.distributions as distributions
-
+import numpy as np
 class EBMRegression(nn.Module):
     def __init__(self, energy, proposal, feature_extractor, num_sample_proposal, base_dist = None, explicit_bias = None,  **kwargs):
         super(EBMRegression, self).__init__()
@@ -75,16 +75,14 @@ class EBMRegression(nn.Module):
         return None
         
 
-    def estimate_log_z(self, x, nb_sample = 1000):
-        batch_size = x.shape[0]
+    def estimate_log_z_onepass(self, x_feature, nb_sample = 1000):
+        assert nb_sample <= 2048, "The number of samples should be less than 2000 in the one pass"
         dic_output = {}
-        if self.feature_extractor is not None:
-            x_feature = self.feature_extractor(x)
-        else :
-            x_feature = x
-        samples = self.proposal.sample(x_feature, nb_sample).to(x.device, x.dtype)
-        samples = samples.reshape(x.shape[0]*nb_sample, -1) #(batch_size, num_samples, y_size)
-        x_feature_expanded = x_feature.unsqueeze(1).expand(-1, nb_sample, -1).reshape(x.shape[0]*nb_sample, -1) #(batch_size * num_samples, x_size)
+        
+        batch_size = x_feature.shape[0]
+        samples = self.proposal.sample(x_feature, nb_sample).to(x_feature.device, x_feature.dtype)
+        samples = samples.reshape(x_feature.shape[0]*nb_sample, -1) #(batch_size, num_samples, y_size)
+        x_feature_expanded = x_feature.unsqueeze(1).expand(-1, nb_sample, -1).reshape(x_feature.shape[0]*nb_sample, -1) #(batch_size * num_samples, x_size)
 
         energy_samples = self.energy(x_feature_expanded, samples).view(batch_size, nb_sample, -1).sum(2) #(batch_size, num_samples)
         dic_output['f_theta_samples'] = energy_samples
@@ -99,17 +97,17 @@ class EBMRegression(nn.Module):
             if self.base_dist != self.proposal :
                 base_dist_log_prob = self.base_dist.log_prob(x_feature_expanded, samples).view(batch_size, nb_sample, -1).sum(2) #(batch_size, num_samples)
                 samples_log_prob = self.proposal.log_prob(x_feature_expanded, samples).view(batch_size, nb_sample, -1).sum(2) #(batch_size, num_samples)
-                aux_prob = base_dist_log_prob - samples_log_prob 
-                log_z_estimate = torch.logsumexp(-energy_samples + aux_prob,dim=1) - torch.log(torch.tensor(nb_sample, dtype=x.dtype, device=x.device)) 
+                aux_log_prob = base_dist_log_prob - samples_log_prob 
+                log_z_estimate = torch.logsumexp(-energy_samples + aux_log_prob,dim=1) - torch.log(torch.tensor(nb_sample, dtype=x.dtype, device=x.device)) 
                 z_estimate = log_z_estimate.exp()
-                dic_output.update({"base_dist_log_prob_samples" : base_dist_log_prob, "proposal_log_prob_samples" : samples_log_prob, "aux_prob_samples" : aux_prob})
+                dic_output.update({"base_dist_log_prob_samples" : base_dist_log_prob, "proposal_log_prob_samples" : samples_log_prob, "aux_log_prob_samples" : aux_log_prob})
             else :
-                log_z_estimate = (-energy_samples).logsumexp(dim=1) - torch.log(torch.tensor(nb_sample, dtype=x.dtype, device=x.device))
+                log_z_estimate = (-energy_samples).logsumexp(dim=1) - torch.log(torch.tensor(nb_sample, dtype=x_feature.dtype, device=x_feature.device))
                 z_estimate = log_z_estimate.exp()
         else :
             samples_log_prob = self.proposal.log_prob(x_feature_expanded, samples).view(batch_size, nb_sample, -1).sum(2)
             dic_output.update({"proposal_log_prob_samples" : samples_log_prob,})
-            log_z_estimate = torch.logsumexp(-energy_samples - samples_log_prob, dim=1) - torch.log(torch.tensor(nb_sample, dtype=x.dtype, device=x.device))
+            log_z_estimate = torch.logsumexp(-energy_samples - samples_log_prob, dim=1) - torch.log(torch.tensor(nb_sample, dtype=x_feature.dtype, device=x_feature.device))
             z_estimate = log_z_estimate.exp()
         
         dic_output['log_z_estimate'] = log_z_estimate
@@ -119,9 +117,37 @@ class EBMRegression(nn.Module):
     
 
 
+    def estimate_log_z(self, x, nb_sample = 1000):
+        batch_size = x.shape[0]
+        dic_output = {}
+        if self.feature_extractor is not None:
+            x_feature = self.feature_extractor(x).detach()
+        else :
+            x_feature = x
+        
+        if nb_sample <= 2048:
+            return self.estimate_log_z_onepass(x_feature, nb_sample = nb_sample)
+        else :
+            number_iter = np.ceil(nb_sample / 2048)
+            last_iter_remaining = nb_sample % 2048
+            for k in range(int(number_iter)):
+                current_nb_sample = 2048
+                if k == number_iter - 1 and last_iter_remaining != 0 :
+                    current_nb_sample = last_iter_remaining
+                _, current_dic_output = self.estimate_log_z_onepass(x_feature, nb_sample = current_nb_sample)
+                for key in current_dic_output :
+                    if 'log' in key :
+                        new_value = current_dic_output[key] + torch.log(torch.tensor(current_nb_sample, dtype=x_feature.dtype, device=x_feature.device))
+                    else :
+                        new_value = current_dic_output[key]*torch.tensor(current_nb_sample, dtype=x_feature.dtype, device=x_feature.device)
+                    if key in dic_output :
+                        dic_output[key] = dic_output[key] + new_value
+                    else :
+                        dic_output[key] = new_value
 
+        for key in dic_output :
+            dic_output[key] = dic_output[key] - torch.log(torch.tensor(nb_sample, dtype=x_feature.dtype, device=x_feature.device))
 
-
-
+        return dic_output['log_z_estimate'], dic_output
 
     
