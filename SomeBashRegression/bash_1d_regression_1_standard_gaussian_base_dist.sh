b7f5443cc3b39d49f6d37ab95eaeda4fefd1e6b8
Hugo <hhjs@dtu.dk>
2023-05-08 23:04:15 +0200

    feat : some bash for 1d regression

---
+++ SomeBashRegression/bash_1d_regression_1_standard_gaussian_base_dist.sh
@@ -0,0 +1,39 @@
+python main_trainer_regression.py \
+--yamldataset 'Dataset/MissingDataDataset/YAMLExamples/1d_regression_1.yaml' \
+--yamlebm Model/YAMLREGRESSION/YAMLEBM_1D/self_normalized.yaml Model/YAMLREGRESSION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLREGRESSION/YAMLENERGY/energy_fc_small_no_bias_no_extractor.yaml Model/YAMLREGRESSION/YAMLOPTIMIZATION/adam1e-2.yaml \
+'Model/YAMLREGRESSION/YAMLBASEDIST/proposal.yaml'
+
+python main_trainer_regression.py \
+--yamldataset 'Dataset/MissingDataDataset/YAMLExamples/1d_regression_1.yaml' \
+--yamlebm Model/YAMLREGRESSION/YAMLEBM_1D/nce.yaml Model/YAMLREGRESSION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLREGRESSION/YAMLENERGY/energy_fc_small_no_bias_no_extractor.yaml Model/YAMLREGRESSION/YAMLOPTIMIZATION/adam1e-2.yaml \
+'Model/YAMLREGRESSION/YAMLBASEDIST/proposal.yaml'
+
+
+python main_trainer_regression.py \
+--yamldataset 'Dataset/MissingDataDataset/YAMLExamples/1d_regression_1.yaml' \
+--yamlebm Model/YAMLREGRESSION/YAMLEBM_1D/eubo.yaml Model/YAMLREGRESSION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLREGRESSION/YAMLENERGY/energy_fc_small_no_bias_no_extractor.yaml Model/YAMLREGRESSION/YAMLOPTIMIZATION/adam1e-2.yaml \
+'Model/YAMLREGRESSION/YAMLBASEDIST/proposal.yaml'
+
+
+python main_trainer_regression.py \
+--yamldataset 'Dataset/MissingDataDataset/YAMLExamples/1d_regression_1.yaml' \
+--yamlebm Model/YAMLREGRESSION/YAMLEBM_1D/self_normalized.yaml Model/YAMLREGRESSION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLREGRESSION/YAMLENERGY/energy_fc_small_no_bias.yaml Model/YAMLREGRESSION/YAMLOPTIMIZATION/adam1e-2.yaml \
+'Model/YAMLREGRESSION/YAMLBASEDIST/proposal.yaml'
+
+python main_trainer_regression.py \
+--yamldataset 'Dataset/MissingDataDataset/YAMLExamples/1d_regression_1.yaml' \
+--yamlebm Model/YAMLREGRESSION/YAMLEBM_1D/nce.yaml Model/YAMLREGRESSION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLREGRESSION/YAMLENERGY/energy_fc_small_no_bias.yaml Model/YAMLREGRESSION/YAMLOPTIMIZATION/adam1e-2.yaml \
+'Model/YAMLREGRESSION/YAMLBASEDIST/proposal.yaml'
+
+
+python main_trainer_regression.py \
+--yamldataset 'Dataset/MissingDataDataset/YAMLExamples/1d_regression_1.yaml' \
+--yamlebm Model/YAMLREGRESSION/YAMLEBM_1D/eubo.yaml Model/YAMLREGRESSION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLREGRESSION/YAMLENERGY/energy_fc_small_no_bias.yaml Model/YAMLREGRESSION/YAMLOPTIMIZATION/adam1e-2.yaml \
+'Model/YAMLREGRESSION/YAMLBASEDIST/proposal.yaml'
+
