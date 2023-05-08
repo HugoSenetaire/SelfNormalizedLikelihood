e4431c9ce248750c3a70d18a182a1a5dbf5dc88f
Hugo <hhjs@dtu.dk>
2023-05-08 23:19:17 +0200

    fix : error sign for logprob kl loss

--- Model/Utils/proposal_loss.py
+++ Model/Utils/proposal_loss.py
@@ -2,16 +2,16 @@ def log_prob_loss_regression(log_prob_proposal, log_estimate_z):
     return - (log_prob_proposal).mean()
 
 def kl_loss_regression(log_prob_proposal, log_estimate_z):
-    return - (log_estimate_z).mean()
+    return log_estimate_z.mean()
 
 def log_prob_kl_loss_regression(log_prob_proposal, log_estimate_z):
-    return - (log_prob_proposal + log_estimate_z).mean()
+    return - (log_prob_proposal - log_estimate_z).mean()
 
 def log_prob_loss(log_prob_proposal, log_estimate_z):
     return - (log_prob_proposal).mean()
 
 def kl_loss(log_prob_proposal, log_estimate_z):
-    return - (log_estimate_z).mean()
+    return log_estimate_z.mean()
 
 def log_prob_kl_loss(log_prob_proposal, log_estimate_z):
-    return - (log_prob_proposal + log_estimate_z).mean()
+    return - (log_prob_proposal - log_estimate_z).mean()
