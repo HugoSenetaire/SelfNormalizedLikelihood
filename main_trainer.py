479ab53654a48c94d41d10d1a29d09b5d7b2941c
PaulJeha <paul.jeha@gmail.com>
2023-05-09 09:42:33 +0200

    tiny fix for the previous commit

--- main_trainer.py
+++ main_trainer.py
@@ -1,20 +1,25 @@
-from default_args import default_args_main, check_args_for_yaml
-from Dataset.MissingDataDataset.prepare_data import get_dataset
-from Model.Utils.model_getter import get_model
-from Model.Utils.dataloader_getter import get_dataloader
-from Model.Utils.Callbacks import EMA
-from Model.Utils.plot_utils import plot_energy_2d, plot_images
-from Model.Trainer import dic_trainer
-import pytorch_lightning as pl
 import os
-import torch
-import numpy as np
+
 import matplotlib.pyplot as plt
-try :
+import numpy as np
+import pytorch_lightning as pl
+import torch
+
+from Dataset.MissingDataDataset.prepare_data import get_dataset
+from default_args import check_args_for_yaml, default_args_main
+from Model.Trainer import dic_trainer
+from Model.Utils.Callbacks import EMA
+from Model.Utils.dataloader_getter import get_dataloader
+from Model.Utils.model_getter import get_model
+from Model.Utils.plot_utils import plot_energy_2d, plot_images
+
+try:
     from pytorch_lightning.loggers import WandbLogger
-except :
+except:
     from lighting.pytorch.loggers import WandbLogger
+
 from tensorboardX import SummaryWriter
+
 
 def find_last_version(dir):
     # Find all the version folders
@@ -30,10 +35,7 @@ def find_last_version(dir):
     return last_version
 
 
-
-
-if __name__ == '__main__' :
-
+if __name__ == "__main__":
     parser = default_args_main()
     args = parser.parse_args()
     args_dict = vars(args)
@@ -47,59 +49,63 @@ if __name__ == '__main__' :
         torch.backends.cudnn.benchmark = False
 
     # Get Dataset :
-    complete_dataset, complete_masked_dataset = get_dataset(args_dict,)
-    train_loader = get_dataloader(complete_masked_dataset.dataset_train, args_dict, shuffle = True)
+    complete_dataset, complete_masked_dataset = get_dataset(
+        args_dict,
+    )
+    train_loader = get_dataloader(
+        complete_masked_dataset.dataset_train, args_dict, shuffle=True
+    )
     val_loader = get_dataloader(complete_masked_dataset.dataset_val, args_dict)
     test_loader = get_dataloader(complete_masked_dataset.dataset_test, args_dict)
-    
-    args_dict['input_size'] = complete_dataset.get_dim_input()
 
-    
-    if args_dict['yamldataset'] is not None :
-        name = os.path.basename(args_dict['yamldataset']).split(".yaml")[0]
-        save_dir = os.path.join(args_dict['output_folder'],os.path.basename(args_dict['yamldataset']).split(".yaml")[0])
-    else :
-        name = args_dict['dataset_name']
-        save_dir = os.path.join(args_dict['output_folder'],args_dict['dataset_name'])
+    args_dict["input_size"] = complete_dataset.get_dim_input()
 
+    if args_dict["yamldataset"] is not None:
+        name = os.path.basename(args_dict["yamldataset"]).split(".yaml")[0]
+        save_dir = os.path.join(
+            args_dict["output_folder"],
+            os.path.basename(args_dict["yamldataset"]).split(".yaml")[0],
+        )
+    else:
+        name = args_dict["dataset_name"]
+        save_dir = os.path.join(args_dict["output_folder"], args_dict["dataset_name"])
 
-
-
-    if args_dict['yamlebm'] is not None and len(args_dict['yamlebm'])>0:
-        list_element = args_dict['yamlebm']
+    if args_dict["yamlebm"] is not None and len(args_dict["yamlebm"]) > 0:
+        list_element = args_dict["yamlebm"]
         list_element.sort()
         for element in list_element:
             name = name + "_" + os.path.basename(element).split(".yaml")[0]
-            save_dir = os.path.join(save_dir,os.path.basename(element).split(".yaml")[0])
-    else :
-        name = name + "_" + args_dict['ebm_name']
-        save_dir = os.path.join(save_dir,args_dict['ebm_name'])
+            save_dir = os.path.join(
+                save_dir, os.path.basename(element).split(".yaml")[0]
+            )
+    else:
+        name = name + "_" + args_dict["ebm_name"]
+        save_dir = os.path.join(save_dir, args_dict["ebm_name"])
 
-    args_dict['save_dir'] = save_dir
-
-
+    args_dict["save_dir"] = save_dir
 
     # Get EBM :
-    if args_dict['just_test']:
-        args_dict['ebm_pretraining'] = False
-        args_dict['proposal_pretraining'] = False
-    ebm = get_model(args_dict, complete_dataset, complete_masked_dataset)    
-
-
+    if args_dict["just_test"]:
+        args_dict["ebm_pretraining"] = False
+        args_dict["proposal_pretraining"] = False
+    ebm = get_model(args_dict, complete_dataset, complete_masked_dataset)
 
     # Get Trainer :
-    algo = dic_trainer[args_dict['trainer_name']](ebm = ebm, args_dict = args_dict, complete_dataset=complete_dataset,)
-
+    algo = dic_trainer[args_dict["trainer_name"]](
+        ebm=ebm,
+        args_dict=args_dict,
+        complete_dataset=complete_dataset,
+    )
 
     nb_gpu = torch.cuda.device_count()
     if nb_gpu > 1 and algo.config["MULTIGPU"] != "ddp":
         raise ValueError("You can only use ddp strategy for multi-gpu training")
-    if nb_gpu>1 and algo.config["MULTIGPU"] == "ddp":
+    if nb_gpu > 1 and algo.config["MULTIGPU"] == "ddp":
         strategy = "ddp"
-    else :
+    else:
         strategy = None
-    if nb_gpu > 0 :
-        accelerator = 'gpu'
+    if nb_gpu > 0:
+        accelerator = "gpu"
         devices = [k for k in range(nb_gpu)]
     else:
         accelerator = None
@@ -107,53 +113,72 @@ if __name__ == '__main__' :
 
     # accelerator = 'mps'
 
-
-    
-    if args.load_from_checkpoint or args_dict['just_test']:
-        ckpt_dir = os.path.join(save_dir,'val_checkpoint')        
+    if args.load_from_checkpoint or args_dict["just_test"]:
+        ckpt_dir = os.path.join(save_dir, "val_checkpoint")
         last_checkpoint = os.listdir(ckpt_dir)[-1]
         ckpt_path = os.path.join(ckpt_dir, last_checkpoint)
         print("Loading from checkpoint : ", ckpt_path)
         assert os.path.exists(ckpt_path), "The checkpoint path does not exist"
         algo.load_state_dict(torch.load(ckpt_path)["state_dict"])
-    else :
+    else:
         ckpt_path = None
-    
+
     # Checkpoint callback :
-    checkpoint_callback_val = pl.callbacks.ModelCheckpoint(dirpath=os.path.join(save_dir,'val_checkpoint'), save_top_k=2, monitor="val_loss")
-    checkpoint_callback_train = pl.callbacks.ModelCheckpoint(dirpath=os.path.join(save_dir,"train_checkpoint"), save_top_k=2, monitor="train_loss")
+    checkpoint_callback_val = pl.callbacks.ModelCheckpoint(
+        dirpath=os.path.join(save_dir, "val_checkpoint"),
+        save_top_k=2,
+        monitor="val_loss",
+    )
+    checkpoint_callback_train = pl.callbacks.ModelCheckpoint(
+        dirpath=os.path.join(save_dir, "train_checkpoint"),
+        save_top_k=2,
+        monitor="train_loss",
+    )
     checkpoints = [checkpoint_callback_val, checkpoint_callback_train]
-    if args_dict['decay_ema'] is not None and args_dict['decay_ema'] > 0:
-        ema_callback = EMA(decay = args_dict['decay_ema'])
+    if args_dict["decay_ema"] is not None and args_dict["decay_ema"] > 0:
+        ema_callback = EMA(decay=args_dict["decay_ema"])
         checkpoints.append(ema_callback)
 
-
-
     # Train :
-    trainer = pl.Trainer(accelerator=accelerator,
-                        # logger=logger,
-                        default_root_dir=save_dir,
-                        callbacks=checkpoints,
-                        # devices = len(devices),
-                        strategy = strategy,
-                        precision=16,
-                        max_steps = args_dict['max_steps'],
-                        # resume_from_checkpoint = ckpt_path)
-    
-    if not args_dict['just_test']:
+    trainer = pl.Trainer(
+        accelerator=accelerator,
+        # logger=logger,
+        default_root_dir=save_dir,
+        callbacks=checkpoints,
+        # devices = len(devices),
+        strategy=strategy,
+        precision=16,
+        max_steps=args_dict["max_steps"],
+    )
+    # resume_from_checkpoint = ckpt_path)
+
+    if not args_dict["just_test"]:
         trainer.fit(algo, train_dataloaders=train_loader, val_dataloaders=val_loader)
-        algo.load_state_dict(torch.load(checkpoint_callback_val.best_model_path)["state_dict"])
+        algo.load_state_dict(
+            torch.load(checkpoint_callback_val.best_model_path)["state_dict"]
+        )
 
     trainer.test(algo, dataloaders=test_loader)
-    
+
     if np.prod(complete_dataset.get_dim_input()) == 2:
         samples = algo.samples_mcmc()[0].flatten(1)
-        plot_energy_2d(algo = algo, save_dir = save_dir, samples = [algo.example, algo.example_proposal, samples], samples_title= ['Samples from dataset', 'Samples from proposal', 'Samples HMC'],)
-    else :
+        plot_energy_2d(
+            algo=algo,
+            save_dir=save_dir,
+            samples=[algo.example, algo.example_proposal, samples],
+            samples_title=[
+                "Samples from dataset",
+                "Samples from proposal",
+                "Samples HMC",
+            ],
+        )
+    else:
         images = algo.samples_mcmc()
-        plot_images(images, save_dir, algo = None, transform_back=complete_dataset.transform_back, name = 'samples_best', step='', )
-   
-
-
-
-        
+        plot_images(
+            images,
+            save_dir,
+            algo=None,
+            transform_back=complete_dataset.transform_back,
+            name="samples_best",
+            step="",
+        )
