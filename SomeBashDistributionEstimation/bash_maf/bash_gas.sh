7ab571f15552e715b056d670d7ec4c6d6f8c9d4d
Hugo <hhjs@dtu.dk>
2023-05-16 00:32:02 +0200

    feat : some bash for maf

--- SomeBashDistributionEstimation/bash_maf/bash_gas.sh
+++ SomeBashDistributionEstimation/bash_maf/bash_gas.sh
@@ -1,34 +1,44 @@
-CUDA_VISIBLE_DEVICES=1 python main_trainer.py \
+CUDA_VISIBLE_DEVICES=6 python main_trainer.py \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
---yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized.yaml Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
-
-python main_trainer.py \
---yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
---yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized.yaml Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
-
-
-python main_trainer.py \
---yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
---yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/normal_diag.yaml \
-Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_adaptive.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
-Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
-
-python main_trainer.py \
---yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
---yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/gaussian_mixture_5.yaml \
-Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_adaptive.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
-Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
-
-python main_trainer.py \
---yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
---yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/normal_diag.yaml \
+--yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
 Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized_train_proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_10.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/kde.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_very_deep.yaml \
+Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-4.yaml
+
+
+
+CUDA_VISIBLE_DEVICES=6 python main_trainer.py --proposal_pretraining True \
+--yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
+--yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized_train_proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/student.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_very_deep.yaml \
+Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-4.yaml
+
+
+CUDA_VISIBLE_DEVICES=6 python main_trainer.py --proposal_pretraining True \
+--yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
+--yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
+Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized_train_proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/student.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_very_deep.yaml \
+Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-4.yaml
+
+
+CUDA_VISIBLE_DEVICES=6 python main_trainer.py \
+--yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
+--yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized_train_proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_very_deep.yaml \
+Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-4.yaml
+
+
+CUDA_VISIBLE_DEVICES=6 python main_trainer.py \
+--yamldataset Dataset/MissingDataDataset/YAMLExamples/gas_maf.yaml \
+--yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
+Model/YAMLDISTRIBUTION/YAMLMAF/self_normalized_train_proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_very_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-4.yaml
