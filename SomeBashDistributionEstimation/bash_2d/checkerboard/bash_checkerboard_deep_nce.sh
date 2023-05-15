e6fae0f7790703ceb03fef0b4349fdae35461be9
Hugo <hhjs@dtu.dk>
2023-05-15 18:18:28 +0200

    feat : bashes for evaluvation of the 2d datasets

--- SomeBashDistributionEstimation/bash_2d/checkerboard/bash_checkerboard_deep_nce.sh
+++ SomeBashDistributionEstimation/bash_2d/checkerboard/bash_checkerboard_deep_nce.sh
@@ -1,70 +1,74 @@
-python main_trainer.py \
+
+
+
+python main_trainer.py --seed 0 --train_base_dist False \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized_train_proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_10.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
-python main_trainer.py \
+
+python main_trainer.py -seed 0 \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized_train_proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_10.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
-python main_trainer.py \
+
+
+python main_trainer.py --seed 1 --train_base_dist False \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_10.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
-python main_trainer.py \
+
+python main_trainer.py -seed 1 \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized.yaml \
-Model/YAMLDISTRIBUTION/YAMLPROPOSAL/gaussian_mixture_10.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
+Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
 
-
-python main_trainer.py \
+python main_trainer.py --seed 2 --train_base_dist False \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized_train_proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
 Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
 
-python main_trainer.py \
+python main_trainer.py -seed 2 \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized_train_proposal.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
 Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
 
-
-python main_trainer.py \
+python main_trainer.py --seed 3 --train_base_dist False \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/proposal.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
 Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
 
 
-python main_trainer.py \
+python main_trainer.py -seed 3 \
 --yamldataset Dataset/MissingDataDataset/YAMLExamples/checkerboard.yaml \
 --yamlebm Model/YAMLDISTRIBUTION/YAMLBASEDIST/none.yaml \
-Model/YAMLDISTRIBUTION/YAMLEBM_2D/self_normalized.yaml \
+Model/YAMLDISTRIBUTION/YAMLEBM_2D/nce.yaml \
 Model/YAMLDISTRIBUTION/YAMLPROPOSAL/standard_gaussian.yaml \
-Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias.yaml \
+Model/YAMLDISTRIBUTION/YAMLENERGY/energy_fc_nobias_deep.yaml \
 Model/YAMLDISTRIBUTION/YAMLOPTIMIZATION/adam1e-3.yaml
-
