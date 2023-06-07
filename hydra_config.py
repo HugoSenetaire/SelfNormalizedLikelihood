016959111a4c0870f2af1b77cd4473e195e1a747
PaulJeha <paul.jeha@gmail.com>
2023-06-07 17:20:58 +0200

    First commit of hydra features

---
+++ hydra_config.py
@@ -0,0 +1,100 @@
+import logging
+from dataclasses import dataclass
+from typing import Any, Optional, Union
+
+from hydra.core.config_store import ConfigStore
+from omegaconf import MISSING
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s %(message)s",
+    datefmt="[%Y-%m-%d %H:%M:%S]",
+)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class BaseBaseDistributionConfig:
+    base_dist_name: str = MISSING
+    train_base_dist: bool = MISSING
+
+
+@dataclass
+class BaseDistributionDatasetConfig:
+    download: bool = MISSING
+    dataset_name: str = MISSING
+    missing_mechanism: str = MISSING
+
+
+@dataclass
+class BaseRegressionDatasetConfig:
+    pass
+
+
+@dataclass
+class BaseEnergyDistributionConfig:
+    energy_name: str = MISSING
+
+
+@dataclass
+class BaseEnergyRegressionConfig:
+    energy_name: str = MISSING
+
+
+@dataclass
+class BaseOptimConfig:
+    optimizer: str = MISSING
+
+
+@dataclass
+class AdamwConfig(BaseOptimConfig):
+    optimizer: str = MISSING
+    lr: float = MISSING
+    weight_decay: float = MISSING
+    b1: float = MISSING
+    b2: float = MISSING
+    eps: float = MISSING
+
+
+@dataclass
+class BaseProposalConfig:
+    proposal_name: str = MISSING
+    num_sample_proposal: int = MISSING
+    num_sample_proposal_val: int = MISSING
+    num_sample_proposal_test: int = MISSING
+    train_proposal: bool = MISSING
+    proposal_loss_name: str = MISSING
+    proposal_pretraining: str = MISSING
+
+    def __post_init__(self):
+        if self.proposal_loss_name not in ["log_prob", "kl", "log_prob_kl"]:
+            raise RuntimeError(
+                f"proposal_loss_name should be in ['log_prob', 'kl', 'log_prob_kl'] but got {self.proposal_loss_name}"
+            )
+
+
+@dataclass
+class BaseTrainConfig:
+    trainer_name: str = MISSING
+    max_steps: int = MISSING
+    batch_size: int = MISSING
+    num_workers: int = MISSING
+    output_folder: str = MISSING
+    load_from_checkpoint: bool = MISSING
+    just_test: bool = MISSING
+    seed: int = MISSING
+    decay_ema: Optional[float] = MISSING
+
+
+def main():
+    cs = ConfigStore.instance()
+    cs.store(
+        name="proposal_name", group="base_distribution", node=BaseBaseDistributionConfig
+    )
+    cs.store(
+        name="checkboard_name",
+        group="dataset/distribution",
+        node=BaseDistributionDatasetConfig,
+    )
+    cs.store(name="adamw_name", group="optim", node=AdamwConfig)
+    cs.store(name="gaussian_name", group="proposal", node=BaseProposalConfig)
