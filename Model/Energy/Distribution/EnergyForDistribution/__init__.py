1ca7e3a88832396fb5d79946d530c616f686d3ad
HugoSenetaire <hhjs@dtu.dk>
2023-08-08 13:38:55 +0200

    feat : add dcgansnv2

--- Model/Energy/Distribution/EnergyForDistribution/__init__.py
+++ Model/Energy/Distribution/EnergyForDistribution/__init__.py
@@ -1,9 +1,12 @@
 from .categorical import get_EnergyCategoricalDistrib
 from .conv import get_ConvEnergy
 from .dcgan_discriminator import get_BNDC_GAN_Discriminator, get_DC_GAN_Discriminator
-from .dcgan_discriminator_SN import get_DC_GAN_DiscriminatorSN
+from .dcgan_discriminator_SN import (
+    get_DC_GAN_DiscriminatorSN,
+    get_DC_GAN_DiscriminatorSNv2,
+)
 from .ising import get_EnergyIsing
-from .linear import get_fc_energy
+from .linear import get_fc_energy, get_fc_energy_sn
 from .poisson import get_EnergyPoissonDistribution
 from .rbm import get_EnergyRBM
 from .resnet_discriminator import get_ResNetDiscriminator
