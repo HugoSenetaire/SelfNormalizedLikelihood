727aa7e7cbb0702d60ac88ed98890813f7769d3d
Hugo <hhjs@dtu.dk>
2023-12-23 11:35:45 +0100

    feat : store the full ais path in list

--- Model/EBMsAndMethod/DistributionEstimation/ais_importance_weighted_ebm.py
+++ Model/EBMsAndMethod/DistributionEstimation/ais_importance_weighted_ebm.py
@@ -76,9 +76,8 @@ class AISImportanceWeightedEBM(ImportanceWeightedEBM):
         value = min(1.0, k / (self.nb_transitions_ais))
         return lambda x: -self.calculate_energy(x, use_base_dist=True)[0].flatten() * value + self.proposal.log_prob(x).flatten() * (1-value)
 
-
-            
-    def sample_ais(self, nb_sample=1, return_log_prob=False, detach_sample = True):
+  
+    def sample_ais(self, nb_sample=1, return_log_prob=False, detach_sample = True, return_full_list=False):
         """
         Samples from the proposal distribution.
         """
@@ -88,38 +87,46 @@ class AISImportanceWeightedEBM(ImportanceWeightedEBM):
         else :
             samples_proposal = self.proposal.sample(nb_sample, return_log_prob = False)
         samples_proposal = samples_proposal.to(next(self.parameters()).device)
-        
         if detach_sample:
             samples_proposal = samples_proposal.detach()
 
     
         samples_proposal.requires_grad_(True)
-        dist = torch.distributions.Normal(0, 1)
+        dist = torch.distributions.Normal(0, 1) 
+        listes_samples_proposal = [samples_proposal]
+        if return_log_prob :
+            liste_samples_proposal_log_prob = [samples_proposal_log_prob]
+
         for k in range(1,self.nb_transitions_ais+1):
             eps = torch.randn_like(samples_proposal)
             target_density = self.get_target(k)
-            density_init = target_density(samples_proposal)
-            forward_grad = torch.autograd.grad(density_init.sum(), samples_proposal, create_graph=True, only_inputs=True)[0]
+            density_init = target_density(listes_samples_proposal[-1])
+            forward_grad = torch.autograd.grad(density_init.sum(), listes_samples_proposal[-1], create_graph=True, only_inputs=True)[0]
             # forward_grad = clip_grad(forward_grad, clip_max_norm=self.clip_max_norm_ais, clip_max_value=self.clip_max_value_ais)
             
 
             update = math.sqrt(2 * self.step_size_ais) * eps + self.step_size_ais * forward_grad
-            new_samples_proposal = samples_proposal + update
+            listes_samples_proposal.append(listes_samples_proposal[-1] + update)
             if return_log_prob :
-                density_step = target_density(new_samples_proposal)
-                backward_grad = torch.autograd.grad(density_step.sum(), new_samples_proposal, create_graph=True, only_inputs=True)[0]
-                eps_reverse = (samples_proposal - new_samples_proposal - self.step_size_ais * backward_grad) / math.sqrt(2 * self.step_size_ais)
-                samples_proposal_log_prob = samples_proposal_log_prob - dist.log_prob(eps_reverse.flatten(1)).sum(1) + dist.log_prob(eps.flatten(1)).sum(1)
-                samples_proposal = new_samples_proposal
+                density_step = target_density(listes_samples_proposal[-1])
+                backward_grad = torch.autograd.grad(density_step.sum(), listes_samples_proposal[-1], create_graph=True, only_inputs=True)[0]
+                eps_reverse = (listes_samples_proposal[-2] - listes_samples_proposal[-1] - self.step_size_ais * backward_grad) / math.sqrt(2 * self.step_size_ais)
+                backward_prob = dist.log_prob(eps_reverse.flatten(1)).sum(1,keepdim=True).reshape(liste_samples_proposal_log_prob[-1].shape)
+                forward_prob = dist.log_prob(eps.flatten(1)).sum(1, keepdim=True).reshape(liste_samples_proposal_log_prob[-1].shape)
+                liste_samples_proposal_log_prob.append(liste_samples_proposal_log_prob[-1] - backward_prob + forward_prob)
             else :
-                samples_proposal = new_samples_proposal.detach()
-                samples_proposal.requires_grad_(True)
+                listes_samples_proposal[-1] = listes_samples_proposal[-1].detach()
+                listes_samples_proposal[-1].requires_grad_(True)
 
+        if return_full_list:
+            if return_log_prob :
+                return listes_samples_proposal, liste_samples_proposal_log_prob
+            return listes_samples_proposal
 
         if return_log_prob :
-            return samples_proposal, samples_proposal_log_prob
+            return listes_samples_proposal[-1], liste_samples_proposal_log_prob[-1]
         else :
-            return samples_proposal
+            return listes_samples_proposal[-1]
 
     def estimate_log_z(
         self,
