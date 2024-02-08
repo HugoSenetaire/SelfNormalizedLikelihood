54c248407de03cb3713d2301ed2f7e121110f857
Hugo Senetaire <hhjs@dtu.dk>
2024-02-08 14:02:00 +0100

    feat : change default sh files

---
+++ sh_files/checkerboard/checkerboard_ais_no_base.sh
@@ -0,0 +1,21 @@
+#!/bin/bash	
+#SBATCH --job-name=checkerboard_ais_no_base
+#SBATCH --account dd-23-138
+#SBATCH --output=checkerboard_ais_no_base-%J.log
+#SBATCH --time=24:00:00
+#SBATCH --partition=qgpu
+#SBATCH --gpus 1
+
+## INFO
+echo "Node: $(hostname)"
+echo "Start: $(date +%F-%R:%S)"
+echo -e "Working dir: $(pwd)\n"
+
+source ~/.bashrc
+conda activate /scratch/project/dd-23-138/conda_envs/pytorch_env
+python main_trainer_checkerboard.py --multirun train=self_normalized_distribution \
+base_distribution=no_base_distribution \
+ebm.train_ais=True train.max_epochs=10 \
+ebm.nb_transitions_ais=2,10,25,50 ebm.step_size_ais=0.005,0.1,1.0 \
+ebm.nb_step_ais=1 ebm.sigma_ais=1.0,0.1,0.01
+echo "Done: $(date +%F-%R:%S)"
---
+++ sh_files/checkerboard/fullshort_term_checkerboard_noais_no_base.sh
@@ -0,0 +1,22 @@
+#!/bin/bash	
+#SBATCH --job-name=checkerboard_fullshort_term_noais_nobase
+#SBATCH --account dd-23-138
+#SBATCH --output=checkerboard_fullshort_term_noais_nobase-%J.log
+#SBATCH --time=24:00:00
+#SBATCH --partition=qgpu
+#SBATCH --gpus 1
+
+## INFO
+echo "Node: $(hostname)"
+echo "Start: $(date +%F-%R:%S)"
+echo -e "Working dir: $(pwd)\n"
+
+source ~/.bashrc
+conda activate /scratch/project/dd-23-138/conda_envs/pytorch_env
+python main_trainer_checkerboard.py --multirun \
+base_distribution=no_base_distribution \
+train=self_normalized_short_term train.start_with_short_term=10000 \
+ebm.train_ais=True train.max_epochs=20 \
+ebm.nb_step_ais=2,10,25,50 ebm.step_size_ais=0.005,0.1,1.0 \
+ebm.nb_transitions_ais=1 ebm.sigma_ais=1.0,0.1,0.01
+echo "Done: $(date +%F-%R:%S)"
